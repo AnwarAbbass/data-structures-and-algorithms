40ef0e3b54a9b42d3baabec24cdb2e6548080cd1
AnwarAbbass <9982017726.UPS@htu.edu.jo>
2021-05-22 23:13:00 +0300

    done

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -7,4 +7,42 @@ describe('Linked List', () => {
   it('works', () => {
     expect(true).toBeTruthy();
   });
+
+  let list;
+  it('create linked list', () => {
+    list = new LinkedLilst()
+    expect(list.head).toBeNull();
+  });
+
+  it('should add new head to empty list', () => {
+    let val = 'a';
+
+    list.insert(val);
+
+    expect(list.head.data).toEqual(val);
+    expect(list.head.next).toBeNull();
+  });
+
+  it('should add new head to unempty list', () => {
+    let val = 'b';
+
+    list.insert(val);
+
+    expect(list.head.data).toEqual(val);
+    expect(list.head.next.next).toBeNull();
+  });
+
+  it('should check if the value is exist', () => {
+    let val = 'b';
+    let val2='r'
+
+    expect(list.includes(val)).toBe(true);
+    expect(list.includes(val2)).toBe(false);
+  });
+
+  it('should return linked list ', () => {
+
+    expect(list.toString()).toEqual('a --> b --> Null ');
+  });
+
 });
--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,6 +1,77 @@
 'use strict';
 
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
 class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    insert(data) {
+        try {
+            let node = new Node(data);
+            if (!this.head) {
+                this.head = node;
+            }
+            else {
+                node.next = this.head;
+                this.head = node;
+            }
+        } catch (error) {
+            console.log('error' + error);
+
+        }
+
+    }
+
+    includes(data) {
+
+        try {
+            let found = false;
+            if (this.head) {
+                node = this.head;
+                while (node.next) {
+                    if (node.data === data) {
+                        found = true;
+                        break;
+                    }
+                    node = node.next;
+                }
+            }
+            return found;
+
+        } catch (error) {
+            console.log('error' + error);
+        }
+    }
+
+    toString() {
+        try {
+
+            let temp = '';
+            node = this.head;
+            while (node.next) {
+
+                if (node.next) {
+                    temp += `${node.data} --> `;
+                }
+                else {
+                    temp += `${node.data} --> Null `;
+                }
+            }
+
+            return temp;
+
+        } catch (error) {
+            console.log('error' + error);
+
+        }
+    }
 
 }
 
