210381a7da0253389a6bad1dd5b7820df0d9eded
AnwarAbbass <9982017726.UPS@htu.edu.jo>
2021-05-25 18:02:49 +0300

    done

--- javascript/shift/__test__/shift.test.js
+++ javascript/shift/__test__/shift.test.js
@@ -3,10 +3,10 @@
 const insertShiftArray = require('../shift-array');
 
 describe('Testing challenge 2', () => {
-    test('It should return reverse array', () => {
-        const test1 =[2,4,6,-8];
+  test('It should shift array from the meddle', () => {
+    const test1 =[2,4,6,-8];
 
-        expect(insertShiftArray(test1, 5)).toEqual([2,4,5,6,-8]);
+    expect(insertShiftArray(test1, 5)).toEqual([2,4,5,6,-8]);
 
-    });
+  });
 });
